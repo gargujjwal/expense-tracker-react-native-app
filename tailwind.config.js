35b4a58240367154dbf44c70ad768e9332fbdbad
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-09-08 13:37:52 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		screens: {
+			sm: "380px",
+			md: "420px",
+			lg: "680px",
+			tablet: "1024px",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
